90b4d7ab295265008d574658acd276d9368f4c34
Simon Pezold <simon.pezold@unibas.ch>
2025-09-15 18:13:01 +0200

    Initial commit of util

---
+++ src/fancy_barchart/util.py
@@ -0,0 +1,8 @@
+from typing import Any, TypeVar
+
+A: TypeVar = TypeVar("A")
+B: TypeVar = TypeVar("B")
+
+def alternate(a: A, b: A, /, *, num: int) -> list[A | B]:
+    """Return ``a`` and ``b`` as an ``num``-element alternated list: ``[a, b, a, b, a, …]``"""
+    return ([a, b] * ((num + 1) // 2))[:num]
