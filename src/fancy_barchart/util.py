2b3634e99a293ee9c57dcd2fbe582d9a950f869c
Simon Pezold <simon.pezold@unibas.ch>
2025-09-16 14:54:35 +0200

    Remove unused import

--- src/fancy_barchart/util.py
+++ src/fancy_barchart/util.py
@@ -1,7 +1,9 @@
-from typing import Any, TypeVar
+from typing import TypeVar
+
 
 A: TypeVar = TypeVar("A")
 B: TypeVar = TypeVar("B")
+
 
 def alternate(a: A, b: A, /, *, num: int) -> list[A | B]:
     """Return ``a`` and ``b`` as an ``num``-element alternated list: ``[a, b, a, b, a, …]``"""
